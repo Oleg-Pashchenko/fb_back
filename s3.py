e470a7054fc00841aab1f1273c5e2af39b4a505b
olegpasenko <odpash.itmo@gmail.com>
2024-06-26 17:26:42 +0300

    init

--- s3.py
+++ s3.py
@@ -46,6 +46,7 @@ def copy_image_to_clipboard(image_path):
     image.convert("RGB").save(output, "BMP")
     data = output.getvalue()[14:]
     output.close()
+
     win32clipboard.OpenClipboard()
     win32clipboard.EmptyClipboard()
     win32clipboard.SetClipboardData(win32clipboard.CF_DIB, data)
