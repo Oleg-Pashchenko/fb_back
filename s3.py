33981ede89c23bc1e7c08860614d46ef1fe6ae1a
olegpasenko <odpash.itmo@gmail.com>
2024-06-26 17:19:06 +0300

    init

--- s3.py
+++ s3.py
@@ -4,6 +4,7 @@ import dotenv
 from PIL import Image
 from io import BytesIO
 import io
+import pyperclip as pc
 
 dotenv.load_dotenv()
 
@@ -36,8 +37,20 @@ def get_object_from_s3(object_name):
         return None
 
 def copy_image_to_clipboard(image_path):
-    command = f"powershell Set-Clipboard -LiteralPath {image_path}"
-    os.system(command)
+    from io import BytesIO
+    import win32clipboard
+    from PIL import Image
+    image = Image.open(image_path)
+
+    output = BytesIO()
+    image.convert("RGB").save(output, "BMP")
+    data = output.getvalue()[14:]
+    output.close()
+    win32clipboard.OpenClipboard()
+    win32clipboard.EmptyClipboard()
+    win32clipboard.SetClipboardData(win32clipboard.CF_DIB, data)
+    win32clipboard.CloseClipboard()
+
 
 # Пример использования функции
 object_url = 'https://s3.timeweb.cloud/931dbb93-olegpash/Снимок экрана 2024-06-24 в 16.30.14.png'
